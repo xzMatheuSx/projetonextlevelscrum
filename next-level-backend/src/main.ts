1f2eeca454bbc85d101b40e2312de8e890ce3780
xzMatheuSx <matheusaugusth@gmail.com>
2025-04-01 17:17:45 -0300

    teste

--- next-level-backend/src/main.ts
+++ next-level-backend/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  const port = process.env.PORT || 3000; // Usa a porta do Render ou 3000 localmente
-  await app.listen(port);
-  console.log(`Aplicação rodando na porta ${port}`); // Log para verificar a porta
+  // back-end/src/main.ts
+app.enableCors({
+  origin: ['https://projetonextlevelscrum.onrender.com']
+});
 }
 bootstrap();
